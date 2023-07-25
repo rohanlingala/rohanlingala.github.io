2624adc86dd842a804656bdc87e17082564e3662
rohanlingala <96801953+rohanlingala@users.noreply.github.com>
2023-07-25 18:20:15 -0500

    Add files via upload

--- checker.js
+++ checker.js
@@ -17,6 +17,16 @@ if(ua.os.family !== null) {
     }
     if(ua.device.family === 'Android'){
         console.log("anroidf")
+        console.log("we are on android baby");
+        const styleElement = document.getElementById("mainStyle")
+        var stylePage = document.getElementById("mainStyle");
+        var fileName = stylePage.getAttribute("href");
+        var newFileName = "";
+        newFileName = "mobile.css";
+
+        stylePage.setAttribute("href", newFileName);
+        styleElement.setAttribute("href",newFileName);
+        localStorage.setItem("stylesheet",newFileName);
         //TODO check 
     }  
     if(ua.device.family === 'Mac OS X'){
