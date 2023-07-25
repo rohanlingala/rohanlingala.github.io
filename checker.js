e8e24a03f22be8126618598ba8a1f5462cd732b2
rohanlingala <96801953+rohanlingala@users.noreply.github.com>
2023-07-25 18:44:25 -0500

    Add files via upload

--- checker.js
+++ checker.js
@@ -1,6 +1,24 @@
 const ua = detect.parse(navigator.userAgent);
+let x = ua.os.family;
+console.log(ua.os.family);
 
 if(ua.os.family !== null) {   
+    console.log(x);
+    console.log(ua.os.family);
+
+    if(ua.os.family === 'Android'){
+        console.log("anroidf")
+        console.log("we are on android baby");
+        const styleElement = document.getElementById("mainStyle")
+        var stylePage = document.getElementById("mainStyle");
+        var fileName = stylePage.getAttribute("href");
+        var newFileName = "";
+        newFileName = "mobile.css";
+
+        stylePage.setAttribute("href", newFileName);
+        styleElement.setAttribute("href",newFileName);
+        localStorage.setItem("stylesheet",newFileName);
+    }  
 
     if(ua.device.family === 'iPhone'){
         //TODO check 
@@ -15,20 +33,7 @@ if(ua.os.family !== null) {
         styleElement.setAttribute("href",newFileName);
         localStorage.setItem("stylesheet",newFileName);
     }
-    if(ua.device.family === 'Android'){
-        console.log("anroidf")
-        console.log("we are on android baby");
-        const styleElement = document.getElementById("mainStyle")
-        var stylePage = document.getElementById("mainStyle");
-        var fileName = stylePage.getAttribute("href");
-        var newFileName = "";
-        newFileName = "mobile.css";
 
-        stylePage.setAttribute("href", newFileName);
-        styleElement.setAttribute("href",newFileName);
-        localStorage.setItem("stylesheet",newFileName);
-        //TODO check 
-    }  
     if(ua.device.family === 'Mac OS X'){
         //TODO check 
         console.log("we are on mac baby");
