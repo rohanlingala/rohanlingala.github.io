1257fe729ed85c4e87ddb1a76bf55afe6a207ed6
rohanlingala <96801953+rohanlingala@users.noreply.github.com>
2023-07-25 19:10:24 -0500

    Add files via upload

--- switchcopy.js
+++ switchcopy.js
@@ -1,14 +1,6 @@
 function toggleStyleSheet(){
 
-    const styleElement = document.getElementById("mainStyle")
-    var stylePage = document.getElementById("mainStyle");
-    var fileName = stylePage.getAttribute("href");
-    var newFileName = "";
-    if(fileName == "css_files/stylecopy.css") newFileName = "css_files/style2.css";
-    else newFileName = "css_files/stylecopy.css";
-    stylePage.setAttribute("href", newFileName);
-    styleElement.setAttribute("href",newFileName);
-    localStorage.setItem("stylesheet",newFileName);
+    onclick="location.href='blog.html';"
 
 }
 
